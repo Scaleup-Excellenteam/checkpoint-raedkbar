5ad960cc9e835b286586645d92ddbb007fa82e87
raedb <raedkbar@gmail.com>
2023-07-26 14:19:34 +0300

    added a func to parse data

--- students.c
+++ students.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define MAX_LINE_LEN 256
 #define NAME_LEN 10
 #define CELL_NUM 10
 #define NUM_COURSES 10
@@ -24,12 +26,23 @@ static struct school S;
 
 void init_db(FILE* file) {
 
+
+    char line[MAX_LINE_LEN];
+    while (fgets(line, sizeof(line), file)) {
+        line[strcspn(line, "\n")] = '\0';
+
+        char *word = strtok(line, " ");
+        while (word != NULL) {
+            printf("%s\n", word);
+            word = strtok(NULL, " ");
+        }
+    }
 }
 
 
 int main() {
     FILE* file;
-    char filename[] = "students_with_class.txt";
+    char filename[] = "C:/Users/raedb/Documents/GitHub/checkpoint_raedkbar/students_with_class.txt";
 
     file = fopen(filename, "r"); // Open the file in write mode
     if (file == NULL) {
